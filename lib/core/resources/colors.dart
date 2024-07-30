e00459dbb2017ba5424e280a70b6e66ac15ab273
duaa <duaasalama22@gmail.com>
2024-07-30 19:35:53 +0300

    first commit

---
+++ lib/core/resources/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class color {
+  static const primaryColor = Color(0xff008955);
+  static const greenCard = Color(0xff08B783);
+  static const greenRectangle = Color(0xffB9E5D1);
+  static const BlackTitlePage = Color(0xff5A5A5A);
+  static const greySubtitle = Color(0xffB8B8B8);
+  static const greyDescription = Color(0xffB8B8B8
+);
+}
---
+++ lib/core/resources/string.dart
@@ -0,0 +1,20 @@
+class StringManger {
+  String anywhereyouare = 'Anywhere you are';
+  String place =
+      'Sell houses easily with the help of\n Listenoryx and to make this line big\nI am writing more';
+  String atanytime = 'At anytime';
+  String time =
+      'Sell houses easily with the help of\n Listenoryx and to make this line big\n I am writing more';
+
+  String bookyourcar = 'Book your car';
+  String descriptionbookcar =
+      'Sell houses easily with the help of\n Listenoryx and to make this line big\n I am writing more.';
+  String enableyourlocation = 'Enable your location';
+  String location ='Choose your location to start find the\n request around you';
+  String welcome = 'Welcome';
+  String usemylocation ='Use my location';
+  String skipfornow ='Skip for now';
+  String createanaccount ='Create an account';
+  String login = 'Log in';
+  String signUp ='';
+}
