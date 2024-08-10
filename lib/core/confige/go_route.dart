55cdbe3d470b40246054c204b07d45e54762daa4
Kamar <qmrask9@gmail.com>
2024-08-10 13:29:59 +0300

    api with bolc 2

--- lib/core/confige/go_route.dart
+++ lib/core/confige/go_route.dart
@@ -37,7 +37,7 @@ final GoRouter router = GoRouter(
   ),
       GoRoute(
     path: '/requestrent',
-    builder: (context, state) =>  gi RequestRentPage(),
+    builder: (context, state) =>  RequestRentPage(),
   ),
          GoRoute(
     path: '/favourite',
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 // import 'package:flutter/material.dart';
 // //import 'package:go_router/go_router.dart';
 // import 'package:ride_app/core/confige/go_route.dart';
@@ -7,10 +6,18 @@
 // import 'core/confige/hive_init.dart';
 // //import 'core/service/bicycles_service.dart';
 
-// void main() {
-//   openHive();
-//   runApp(MyApp());
-// }
+import 'package:flutter/material.dart';
+
+import 'core/service/bicycles_service.dart';
+import 'core/service/hubs_service.dart';
+import 'src/features/transport/view/available_car_list.dart';
+import 'src/features/transport/view/car_detailes.dart';
+import 'src/features/transport/view/request_rent.dart';
+
+void main() {
+  //openHive();
+  runApp(MyWidget());
+}
 
 // class MyApp extends StatefulWidget {
 //   @override
@@ -27,52 +34,55 @@
 //   }
 // }
 
-// class MyWidget extends StatelessWidget {
-//   const MyWidget({super.key});
 
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:AvailableCarList()
+      // home: Scaffold(
+      //   body: Center(
+      //     child: ElevatedButton(
+      //       child: Text("Test api"),
+      //       onPressed: () {
+      //       BicyclesServiceImp().getAllBicyclesByCategoryName();
+      //         BicyclesServiceImp().getAllBicyclesCategorys();
+      //         HubsServiceImp().getAllHubs();
+      //         HubsServiceImp().getHubById();
+      //         HubsServiceImp().getContentOfHubById();
+      //       },
+      //     ),
+      //   ),
+      // ),
+     );
+  }
+}
+// import 'package:flutter/material.dart';
+// import 'package:ride_app/core/confige/go_route.dart';
+// import 'core/confige/hive_init.dart';
+
+// void main() {
+//   openHive();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+
+// class _MyAppState extends State<MyApp> {
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: ElevatedButton(
-//             child: Text("Test api"),
-//             onPressed: () {
-//             //BicyclesServiceImp().getAllBicyclesByCategoryName();
-//               //BicyclesServiceImp().getAllBicyclesCategorys();
-//               //HubsServiceImp().getAllHubs();
-//               HubsServiceImp().getHubById();
-//               //HubsServiceImp().getContentOfHubById();
-//             },
-//           ),
-//         ),
-//       ),
+//     return MaterialApp.router(
+//       routerConfig: router,
+//       debugShowCheckedModeBanner: false,
+                  
+      
 //     );
 //   }
 // }
-import 'package:flutter/material.dart';
-import 'package:ride_app/core/confige/go_route.dart';
-import 'core/confige/hive_init.dart';
-
-void main() {
-  openHive();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-                  
-      
-    );
-  }
-}
